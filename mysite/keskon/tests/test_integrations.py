355c9ff5efc4bc7852937022c873d2e95887f939
antonyboivin <antonyboivin@outlook.fr>
2019-06-22 19:52:59 +0200

    Add functional tests

--- mysite/keskon/tests/test_integrations.py
+++ mysite/keskon/tests/test_integrations.py
@@ -12,26 +12,28 @@ class StatusCodePages(TestCase):
     """
         Class StatusCodePages ensures that all templates return a status code 200.
     """
-    def setup(self):
-        """
-            The setup method contains :
-                self.usertest creates a user for the test requirements.
-        """
+
+
+    def test_home_page(self):
         user = User.objects.create(username='usertest', password='usertest')
         MealIdea.objects.create(
+            id= 1,
             mealIdeaName= 'test',
             mealIdeaDescription= 'test',
             mealIdeaCategory= 'test',
             mealIdeaPublisher= user,
-            #mealIdeaLikes.set(),
             mealIdeaSteps= 'test',
             mealIdeaImage= 'test'
         )
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
 
     def test_legal_notice_page(self):
         response = self.client.get(reverse('legal_notice'))
         self.assertEqual(response.status_code, 200)
-    
+
+
     def test_my_account_page(self):
         logged = self.client.login(username='usertest', password='usertest')
         if logged:
@@ -39,13 +41,16 @@ class StatusCodePages(TestCase):
             self.assertEqual(response.status_code, 200)
             self.client.logout()
 
+
     def test_display_all_idea_page(self):
         response = self.client.get(reverse('display_all_idea'))
         self.assertEqual(response.status_code, 200)
 
+
     def test_display_by_category_page(self):
         response = self.client.get(reverse('display_by_category', kwargs={'cat': 'viande'}))
         self.assertEqual(response.status_code, 200)
+
 
     def test_ddisplay_categories_page(self):
         list_cat = {
@@ -59,31 +64,36 @@ class StatusCodePages(TestCase):
         response = self.client.get(reverse('display_categories'))
         self.assertEqual(response.status_code, 200)
 
-        """
+
     def test_detail_idea_page(self):
         user = User.objects.create(username='usertest', password='usertest')
-        MealIdea.objects.create(
-            id= 1,
-            mealIdeaName= 'test',
-            mealIdeaDescription= 'test',
-            mealIdeaCategory= 'test',
-            mealIdeaPublisher= user,
-            #mealIdeaLikes.set(),
-            mealIdeaSteps= 'test',
-            mealIdeaImage= 'test'
-        )
-        response = self.client.get('keskon/detail_idea/1/')
-        self.assertEqual(response.status_code, 200)
-                
+        logged = self.client.login(username='usertest', password='usertest')
+        if logged:
+            MealIdea.objects.create(
+                id= 3,
+                mealIdeaName= 'test',
+                mealIdeaDescription= 'test',
+                mealIdeaCategory= 'test',
+                mealIdeaPublisher= user,
+                #mealIdeaLikes.set(),
+                mealIdeaSteps= 'test',
+                mealIdeaImage= 'test'
+            )
+            response = self.client.get('/detail_idea/?id=3')
+            self.assertEqual(response.status_code, 200)
+             
     def test_add_idea_page(self):
-        response = self.client.get(reverse('add_idea'))
-        self.assertEqual(response.status_code, 200)
-        """
-        """
-        # TEST FONCTIONNEL
+        user = User.objects.create(username='usertest', password='usertest')
+        logged = self.client.login(username='usertest', password='usertest')
+        if logged:
+            response = self.client.get(reverse('add_idea'))
+            self.assertEqual(response.status_code, 200)
+
+
     def test_edit_idea_page(self):
         user = User.objects.create(username='usertest', password='usertest')
         MealIdea.objects.create(
+            id= 2,
             mealIdeaName= 'test',
             mealIdeaDescription= 'test',
             mealIdeaCategory= 'test',
@@ -92,14 +102,14 @@ class StatusCodePages(TestCase):
             mealIdeaSteps= 'test',
             mealIdeaImage= 'test'
         )
-        response = self.client.get(reverse('edit_idea', kwargs={'id': 1}))
+        response = self.client.get(reverse('edit_idea', kwargs={'id': 2}))
         self.assertEqual(response.status_code, 200)
-        """
-        """  
-        # TEST FONCTIONNEL
+
+
     def test_delete_idea_page(self):
         user = User.objects.create(username='usertest', password='usertest')
         MealIdea.objects.create(
+            id= 3,
             mealIdeaName= 'test',
             mealIdeaDescription= 'test',
             mealIdeaCategory= 'test',
@@ -107,17 +117,22 @@ class StatusCodePages(TestCase):
             mealIdeaSteps= 'test',
             mealIdeaImage= 'test'
         )
-        response = self.client.get(reverse('delete_idea', kwargs={'id': 1}))
+        response = self.client.get(reverse('delete_idea', kwargs={'id': 3}))
         self.assertEqual(response.status_code, 302)
-        """
-        """
+
+
     def test_likes_idea_page(self):
-        response = self.client.get(reverse('likes_idea'))
-        self.assertEqual(response.status_code, 200)
-        """
+        user = User.objects.create(username='usertest', password='usertest')
+        logged = self.client.login(username='usertest', password='usertest')
+        if logged:
+            response = self.client.get(reverse('likes_idea'))
+            self.assertEqual(response.status_code, 200)
+
+
     def test_week_menu_page(self):
         response = self.client.get(reverse('week_menu'))
         self.assertEqual(response.status_code, 200)
+
 
     def test_delete_week_idea_page(self):
         user = User.objects.create(username='usertest', password='usertest')
@@ -137,6 +152,7 @@ class StatusCodePages(TestCase):
             typeMeal ='Dessert')
         response = self.client.get(reverse('delete_week_idea', kwargs={'id': 1}))
         self.assertEqual(response.status_code, 200)
+
 
     def test_del_all_week_idea_page(self):
         response = self.client.get(reverse('del_all_week_idea'))
